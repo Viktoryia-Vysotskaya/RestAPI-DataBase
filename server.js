6dd46a3a89fac091b26e488ba8fc5fe4cb8dfe95
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-10-09 10:41:31 +0200

    Update files for Replit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
 const db = require('./db');
+const path = require('path');
 
 const app = express();
 
@@ -21,10 +21,17 @@ app.use('/api', concertsRoutes);
 // Add seats routes to server
 app.use('/api', seatsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+  
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
