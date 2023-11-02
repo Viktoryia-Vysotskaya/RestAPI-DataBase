2d098fb7f1a5f22c5b03512ea2e8fd848d98375d
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-11-02 12:50:04 +0100

    Add Snyk package / Update 'build'

--- server.js
+++ server.js
@@ -12,23 +12,7 @@ const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'https://restapi-database.viktoryiavysots.repl.co'
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow external access...';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(helmet());
